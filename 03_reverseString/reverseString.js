7cba9802e4106057b0f1d8859962246d11fbdb65
Cephalophile <109045174+cephalophile@users.noreply.github.com>
2023-05-12 13:54:26 -0400

    completed challenge 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let splitString = word.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
 };
 
 // Do not edit below this line
