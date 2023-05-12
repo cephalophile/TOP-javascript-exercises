d1caec75bab33b64ab2093054f9b820e2b1910a9
Cephalophile <109045174+cephalophile@users.noreply.github.com>
2023-05-12 15:07:28 -0400

    completed challenge 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removed) {
+//convert second argument to an array
+//loop through array and compare to "removed"
+//if i = to "removed" delete from the array
+    //may need to split & join somehow
+//return the remaining array
+    const newArray = [];
+    array.forEach((item) => {
+        if (!removed.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
